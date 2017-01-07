a3273d64bc044885da4531cb1dc976f83213a909
x_studying <xg_55@126.com>
2017-01-07 18:11:54 +0800

    基础代码

---
+++ UL/UL/Metadata.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UL
+{
+    
+    public class Metadata
+    {
+        public string[] MetaStrings;
+    }
+
+    public enum EAccessModifier
+    {
+        Public,
+        Protected,
+        Private
+    }
+
+    public class UL_Member
+    {
+        public EAccessModifier Modifier;
+        public string Type;
+        public string Name;
+        public string OwnerType;
+        public bool IsStatic;
+        public bool IsInternal;
+        public Metadata Metadata;
+    }
+
+    public class UL_Function : UL_Member
+    {
+        public enum EParamModifier
+        {
+            In,
+            Out,
+            Ref
+        }
+        public struct Parameter
+        {
+            public EParamModifier Modifier;
+            public string Type;
+            public string Name;
+        }
+        public Parameter[] Params;
+
+        public bool IsVirsual;
+    }
+
+    public class UL_Variable : UL_Member
+    {
+        public bool can_read;
+        public bool can_write;
+    }
+
+
+    public class UL_Type
+    {
+        public UL_Function[] Functions;
+        public UL_Variable[] Variables;
+        public string Name;
+        public string Parent;
+        public string Outer;
+        public EAccessModifier Modifier;
+    }
+
+    public class Data
+    {
+        public static List<UL_Type> AllTypes;
+        public static Dictionary<string, UL_Type> NameTypeMap;
+    }
+}
---
+++ UL/UL/Runtime.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UL
+{
+    public class Command
+    {
+        public enum ECode
+        {
+            NewObject,
+            Call,
+            List,
+            Loop_For,
+            Loop_WhileDo,
+            Loop_DoWhile,
+            If,
+            Switch,
+            Return
+        }
+
+        public ECode Code;
+    }
+
+    public class Command_NewObject : Command
+    {
+        //New的对象类型
+        public string Type;
+        //新变量名称
+        public string Name;
+
+        public Command_NewObject() { Code = ECode.NewObject; }
+    }
+
+    public class Command_Call : Command
+    {
+        //调用函数的对象，或者类
+        public string Object;
+        //调用的函数名
+        public string Function;
+        //调用的参数
+        public string[] Args;
+
+        public Command_Call() { Code = ECode.Call; }
+    }
+
+    public class Command_Return:Command
+    {
+        public string Object;
+
+        public Command_Return() { Code = ECode.Return; }
+    }
+
+    public class Command_List : Command
+    {
+        public Command[] Body;
+
+        public Command_List() { Code = ECode.List; }
+    }
+
+    public class Command_Loop_For:Command
+    {
+        public string StartObject;
+        public string ConditionObject;
+        public Command End;
+        public Command[] LoopBody;
+
+        public Command_Loop_For() { Code = ECode.Loop_For; }
+    }
+
+    public class Command_Loop_WhileDo:Command
+    {
+        public string ConditionObject;
+        public Command[] Body;
+
+        public Command_Loop_WhileDo() { Code = ECode.Loop_WhileDo; }
+    }
+    public class Command_Loop_DoWhile : Command
+    {
+        public Command[] Body;
+        public string ConditionObject;
+        public Command_Loop_DoWhile() { Code = ECode.Loop_DoWhile; }
+    }
+    public class Command_If : Command
+    {
+        public string ConditionObject;
+        public Command[] TrueBody;
+        public Command[] FalseBody;
+
+        public Command_If() { Code = ECode.If; }
+    }
+
+    public class Command_Switch : Command
+    {
+        public string ConditionObject;
+        public class Case
+        {
+            public string CaseValue;
+            public Command[] Body;
+        }
+        public Case[] CaseList;
+        public Command[] DefaultCase;
+
+        public Command_Switch() { Code = ECode.Switch; }
+    }
+
+    public class VisualMachine
+    {
+        public List<Command> CommandList;
+    }
+}
