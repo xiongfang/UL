03b1ebd8dbfd4fc5df5b5e34a73152351f1ddaf9
xg_55 <xg_55@126.com>
2017-10-02 12:36:30 +0800

    删除不必要的项目

--- UL/UL_CPP_VM/TestMain.cpp
+++
@@ -1,7 +0,0 @@
-#include "TestMain.h"
-#include <stdio.h>
-
-void Test()
-{
-	printf("hello world!");
-}
--- UL/UL_CPP_VM/TestMain.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#define EXPORT extern "C" __declspec(dllexport)
-
-EXPORT void Test();
