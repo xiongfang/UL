1645376eee9c78e0e9811602af4f0dc8c3fa4ead
xg_55@126.com <xg_55@126.com>
2017-11-29 18:11:21 +0800

    文件

---
+++ UL/System/Reflection/TypeFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace System.Reflection
+{
+    public delegate bool TypeFilter(
+        Type m,
+        object filterCriteria
+    );
+
+    public delegate bool MemberFilter(MemberInfo m, object filterCriteria);
+}
