49b6be78c18c0728d1cf5a78a26019e24341451f
xg_55@126.com <xg_55@126.com>
2017-12-19 14:46:31 +0800

    System::ArgumentNullException(new System::String("value"));

--- UL/CppApp/System.cpp
+++ UL/CppApp/System.cpp
@@ -13,6 +13,7 @@
 #include "System/UInt64.h"
 #include "System/Double.h"
 #include "System/Char.h"
+#include "System/ArgumentNullException.h"
 
 using namespace System;
 
@@ -126,6 +127,8 @@ Ref<System::String> Char::ToString()
 
 System::Byte Byte::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	Byte v;
 	wscanf_s(value->c_str(), _T("%d"), &v._v);
 	return v;
@@ -133,6 +136,8 @@ System::Byte Byte::Parse(Ref<System::String>  value)
 
 System::Char Char::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	Char v;
 	short temp;
 	wscanf_s(value->c_str(), _T("%hd"), &temp);
@@ -142,54 +147,72 @@ System::Char Char::Parse(Ref<System::String>  value)
 
 System::Double Double::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	Double v;
 	wscanf_s(value->c_str(), _T("%lf"), &v._v);
 	return v;
 }
 System::Int16 Int16::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	Int16 v;
 	wscanf_s(value->c_str(), _T("%hd"), &v._v);
 	return v;
 }
 System::Int32 Int32::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	Int32 v;
 	wscanf_s(value->c_str(), _T("%d"), &v._v);
 	return v;
 }
 System::Int64 Int64::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	Int64 v;
 	wscanf_s(value->c_str(), _T("%lld"), &v._v);
 	return v;
 }
 System::SByte SByte::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	SByte v;
 	wscanf_s(value->c_str(), _T("%hhd"), &v._v);
 	return v;
 }
 System::Single Single::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	Single v;
 	wscanf_s(value->c_str(), _T("%f"), &v._v);
 	return v;
 }
 System::UInt16 UInt16::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	UInt16 v;
 	wscanf_s(value->c_str(), _T("%hu"), &v._v);
 	return v;
 }
 System::UInt32 UInt32::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	UInt32 v;
 	wscanf_s(value->c_str(), _T("%u"), &v._v);
 	return v;
 }
 System::UInt64 UInt64::Parse(Ref<System::String>  value)
 {
+	if (value.IsNull())
+		throw new System::ArgumentNullException(new System::String("value"));
 	UInt64 v;
 	wscanf_s(value->c_str(), _T("%llu"), &v._v);
 	return v;
